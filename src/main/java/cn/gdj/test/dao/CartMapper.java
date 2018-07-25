23c69c36cdc73e05ae8e0bc4f3a168c3087a6ee2
yuchanglong <yuchanglong@epipechina.com>
2018-07-25 16:12:57 +0800

    mybatis集成以及代码生成器的使用

---
+++ src/main/java/cn/gdj/test/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package cn.gdj.test.dao;
+
+import cn.gdj.test.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
---
+++ src/main/java/cn/gdj/test/dao/PayInfoMapper.java
@@ -0,0 +1,17 @@
+package cn.gdj.test.dao;
+
+import cn.gdj.test.pojo.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
